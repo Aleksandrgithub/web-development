47c937ce665e0e59ee8a368c905a10e1ff25cc14
Aleksandrgithub <61562490+Aleksandrgithub@users.noreply.github.com>
2020-02-28 13:08:20 +0300

    lw3

---
+++ lw3/index.php
@@ -0,0 +1,11 @@
+<?php
+	$_GET;
+	function getGETParametr(string $text):?string
+	{
+		return isset($_GET[$text]) ? (string)$_GET[$text] : null;
+	}
+
+	header("Content-Type: text/plain");
+	$text = getGETParametr('text'); 
+    echo preg_replace('/^([ ]+)|([ ]){2,}/m', '$2', $text);
+    //echo $text = preg_replace('/\s\s+/', ' ', $text);
---
+++ lw3/index2.php
@@ -0,0 +1,19 @@
+<?php	
+	$_GET;
+	function getGETParametr(string $text):?string
+	{
+		return isset($_GET[$text]) ? (string)$_GET[$text] : null;
+	}
+
+	header("Content-Type: text/plain");
+	$text = getGETParametr('text');
+	if (($text == null) || ($text[0] === '1') || ($text[0] === '2') || ($text[0] === '3') || ($text[0] === '4') || ($text[0] == '5') || ($text[0] === '6') || ($text[0] === '7') || ($text[0] === '8') || ($text[0] === '9')) 
+	{
+		echo 'false';
+	}
+	else
+    {
+    	echo 'true';
+    }
+    //echo $text;
+    //echo $text = preg_replace('/\s\s+/', ' ', $text);
