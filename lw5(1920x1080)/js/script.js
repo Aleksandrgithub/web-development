4a658f1f5ae1a3d7c328c9d790e0c2a9cd53237f
Aleksandrgithub <akksdayz@gmail.com>
2020-04-03 04:37:30 -0700

    up

--- lw5(1920x1080)/js/script.js
+++ lw5(1920x1080)/js/script.js
@@ -3,7 +3,7 @@ function backToTop() {
     let button = $('.back-to-top');
 
     $(window).on('scroll', () => {
-        if ($(this).scrollTop() >= 50) {
+        if ($(this).scrollTop() >= 500) {
             button.fadeIn();
         } else {
             button.fadeOut();
