007fcd93c27ee262fbf3b27f9504a7ccbbde4cd4
Aleksandrgithub <akksdayz@gmail.com>
2020-04-03 17:35:35 -0700

    up

--- lw7/lw7_1/script.js
+++ lw7/lw7_1/script.js
@@ -1,37 +1,42 @@
 function isPrimeNumber(a) {
-	isPrime = true
-    if (typeof a == 'number') {
-        for (let j = 2; j < a; j++) {
-        if (a % j == 0) {
-            isPrime = false;
-            break;
-        }
-        }
-        if (isPrime) {
-            console.log(a + ' is prime number')
-        }
-        else {
-            console.log(a + ' is not prime number')
-        }
+  let isPrime = true;
+  if (typeof a == 'number') {
+    for (let j = 2; j < a; j++) {
+      if (a % j == 0) {
+        isPrime = false;
+        break;
+      }
     }
-
-    else {
-        if (typeof a == 'object') {
-            for (let i = 0; i < a.length; i++) {
-                for (let j = 2; j < a[i]; j++) {
-                    if (a[i] % j == 0) {
-                        isPrime = false;
-                        break;
-                    }
-                }
-                if (isPrime) {
-                    console.log(a[i] + ' is prime number')
-                }
-                else {
-                    console.log(a[i] + ' is not prime number')
-                }    
+      if (isPrime) {
+        console.log(a + ' is prime number');
+    } else {
+      console.log(a + ' is not prime number');
+    }
+  } else {
+    if (typeof a == 'object') {
+      if (a.length != 0) {
+        for (let i = 0; i < a.length; i++) {
+          if (typeof a[i] == 'number') {
+            for (let j = 2; j < a[i]; j++) {
+              if (a[i] % j == 0) {
+                isPrime = false;
+                break;
+              }
             }
+            if (isPrime) {
+              console.log(a[i] + ' is prime number')
+            } else {
+              console.log(a[i] + ' is not prime number')
+            }
+          } else {
+            console.log(a[i] + ' invalid array element')
+          }      
         }
+      } else {
+        console.log('array is empty')
+      }
+    } else {
+      console.log(a[i] + ' invalid input')
     }
-    	
+  }     
 }
