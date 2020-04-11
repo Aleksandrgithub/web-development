cfeec092603d7307c39b6315513a932317e12599
Aleksandrgithub <akksdayz@gmail.com>
2020-04-11 07:25:37 -0700

    up

--- lw7/lw7_1/script.js
+++ lw7/lw7_1/script.js
@@ -1,42 +1,44 @@
-function isPrimeNumber(a) {
-  let isPrime = true;
-  if (typeof a == 'number') {
-    for (let j = 2; j < a; j++) {
-      if (a % j == 0) {
-        isPrime = false;
-        break;
-      }
+const num = 'number';
+
+function CheckArgument(value) {
+  isPrime = true;
+  for (let j = 2; j < value; j++) {
+    if (value % j == 0) {
+      isPrime = false;
+      break;
     }
-      if (isPrime) {
-        console.log(a + ' is prime number');
-    } else {
-      console.log(a + ' is not prime number');
-    }
+  }
+}
+
+function CheckIsPrime(value) {
+  if (isPrime) {
+    console.log(value + ' is prime number');
   } else {
-    if (typeof a == 'object') {
-      if (a.length != 0) {
-        for (let i = 0; i < a.length; i++) {
-          if (typeof a[i] == 'number') {
-            for (let j = 2; j < a[i]; j++) {
-              if (a[i] % j == 0) {
-                isPrime = false;
-                break;
-              }
-            }
-            if (isPrime) {
-              console.log(a[i] + ' is prime number')
-            } else {
-              console.log(a[i] + ' is not prime number')
-            }
+    console.log(value + ' is not prime number');
+  }
+}
+
+
+function isPrimeNumber(value) {
+  if (typeof value == num) {
+    CheckArgument(value);
+    CheckIsPrime(value);
+  } else {
+    if (typeof value == 'object') {
+      if (value.length != 0) {
+        for (let i = 0; i < value.length; i++) {
+          if (typeof value[i] == num) {
+            CheckArgument(value[i]);
+            CheckIsPrime(value[i]);
           } else {
-            console.log(a[i] + ' invalid array element')
+            console.log(value[i] + ' invalid array element')
           }      
         }
       } else {
         console.log('array is empty')
       }
     } else {
-      console.log(a[i] + ' invalid input')
+      console.log(value[i] + ' invalid input')
     }
   }     
 }
