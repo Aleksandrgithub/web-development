235c44a96eaca1f929fae735fc6b3e35d9785922
Aleksandrgithub <akksdayz@gmail.com>
2020-06-13 07:56:07 -0700

    up

--- lw3/task3/index3.php
+++ lw3/task3/index3.php
@@ -14,50 +14,59 @@
     $numOfLower = 0;
     $security = 0;
     $passLength= strlen($pass);
-    foreach ($passArray as $elArray) 
-    {
-        if ($elArray === '0' || $elArray === '1' || $elArray === '2' || $elArray === '3' || $elArray === '4' || $elArray === '5' || $elArray === '6' || $elArray === '7' || $elArray === '8' || $elArray === '9' )
+    function digitCheck() {
+        foreach ($passArray as $elArray) 
         {
-            $numOfDigit = $numOfDigit + 1;
-        }
-        if (ctype_upper($elArray))
-        {
-            $numOfUpper = $numOfUpper + 1;
+            if ($elArray === '0' || $elArray === '1' || $elArray === '2' || $elArray === '3' || $elArray === '4' || $elArray === '5' || $elArray === '6' || $elArray === '7' || $elArray === '8' || $elArray === '9' )
+            {
+                $numOfDigit = $numOfDigit + 1;
+            }
+            if (ctype_upper($elArray))
+            {
+                $numOfUpper = $numOfUpper + 1;
 
-        }
-        if (ctype_lower($elArray))
-        {
-            $numOfLower= $numOfLower + 1;
+            }
+            if (ctype_lower($elArray))
+            {
+                $numOfLower= $numOfLower + 1;
 
-        }  
+            }  
+        }
     }
-      
+
+    function repeatCheck() {
+        if (preg_match("/^[a-zA-Z]+$/", $pass))
+        {
+            $security = $security - $passLength;
+        }
+
+        if (preg_match("/^[1-9]+$/", $pass))
+        {
+            $security = $security - $passLength;
+        }
+
+        foreach ($repeatArray as $elArray) 
+        {
+            if ($elArray !== 1)
+            {
+                $security = $security - $elArray;
+            }
+          
+        }
+    }
+
     $security  = $security  + 4*$passLength + $numOfDigit + (($passLength-$numOfUpper)*2) + (($passLength-$numOfLower)*2) ;
 
-    if (preg_match("/^[a-zA-Z]+$/", $pass))
-    {
-        $security = $security - $passLength;
-    }
+    function validCheck($pass, $security) {
 
-    if (preg_match("/^[1-9]+$/", $pass))
-    {
-        $security = $security - $passLength;
-    }
-
-    foreach ($repeatArray as $elArray) 
-    {
-        if ($elArray !== 1)
+        if (preg_match("/^[А-Яа-я\-.]+$/", $pass) || $pass == null)
         {
-            $security = $security - $elArray;
+            echo "Неккоректный пароль";
         }
-      
+        else
+        {
+            echo "Надежность пароля: ", $security;
+        }
     }
-    
-    if (preg_match("/^[А-Яа-я\-.]+$/", $pass) || $pass == null)
-    {
-        echo "Неккоректный пароль";
-    }
-    else
-    {
-        echo "Надежность пароля: ", $security;
-    }
+
+    validCheck($pass, $security);
--- lw3/task4/index4.php
+++ lw3/task4/index4.php
@@ -11,16 +11,20 @@
     $last_name = getGetParameter('last_name');
     $age = getGetParameter('age');
     $file = 'data/' . $email.".txt";
-    if (isset($email) && $email !== '') {
-        if (file_exists($file)) {
-            unlink('data/' . $email.".txt");
+    function makeFile($email, $first_name, $last_name, $age) {
+        if (isset($email) && $email !== '') {
+            if (file_exists($file)) {
+                unlink('data/' . $email.".txt");
+            }
+            $file = fopen('data/' . $email . '.txt', "w+");
+            fwrite($file, 'email: ' . $email . PHP_EOL);
+            fwrite($file, 'first_name: ' . $first_name . PHP_EOL);
+            fwrite($file, 'last_name: ' . $last_name . PHP_EOL);
+            fwrite($file, 'age: ' . $age . PHP_EOL);
+            fclose($file);
+        } else {
+            echo 'Введите email';
         }
-        $file = fopen('data/' . $email . '.txt', "w+");
-        fwrite($file, 'email: ' . $email . PHP_EOL);
-        fwrite($file, 'first_name: ' . $first_name . PHP_EOL);
-        fwrite($file, 'last_name: ' . $last_name . PHP_EOL);
-        fwrite($file, 'age: ' . $age . PHP_EOL);
-        fclose($file);
-    } else {
-        echo 'Введите email';
     }
+
+    makeFile($email, $first_name, $last_name, $age);
